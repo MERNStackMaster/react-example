5bdc66c7db0d0f37bb692f4b8d14ca2e6427ac60
MERN Stack Master <msm@MBP.local>
2020-08-24 14:28:44 -0700

    Updates

---
+++ precache-manifest.db192ec5760e66fe01c75b3bdf605414.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63bf9840938e94b8b4b327ae69cdc98",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "c94001c85ca8cfa89096",
+    "url": "/react-example/static/css/main.9e1e1217.chunk.css"
+  },
+  {
+    "revision": "938c59259704dff72041",
+    "url": "/react-example/static/js/2.7b710fc7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-example/static/js/2.7b710fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94001c85ca8cfa89096",
+    "url": "/react-example/static/js/main.dad03999.chunk.js"
+  },
+  {
+    "revision": "591a35e8bba386688ec8",
+    "url": "/react-example/static/js/runtime-main.c33dc506.js"
+  },
+  {
+    "revision": "87956ea671bb372c72ea20e38ec7944a",
+    "url": "/react-example/static/media/ireland.87956ea6.jpg"
+  }
+]);
